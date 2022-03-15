484fd1aa4655f521e0478386d26565fa11ccdacd
YeonwooSung <neos960518@gmail.com>
2022-03-15 18:20:00 +0900

    Start implementing retro with pytorch

--- pyretro/pyretro.py
+++ pyretro/pyretro.py
@@ -1,5 +1,9 @@
 import torch
 from packaging import version
 from torch import _softmax_backward_data
+import torch.nn.functional as F
+from torch import nn, einsum
 
+from pyretro.retrieval import BERT_VOCAB_SIZE
+from einops import rearrange, repeat
 
