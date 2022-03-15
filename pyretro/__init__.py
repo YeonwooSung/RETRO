a1fe5b66d2d1570fa4e6ba8bd95f474bb4ddf437
YeonwooSung <neos960518@gmail.com>
2022-03-15 23:57:08 +0900

    Implement RETRO with PyTorch

--- pyretro/__init__.py
+++ pyretro/__init__.py
@@ -5,3 +5,7 @@ from .utils import logging
 
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
+
+from pyretro.pyretro import RETRO
+from pyretro.data import RETRODataset
+from pyretro.training_pytorch import TrainingWrapper
