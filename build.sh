0c2be7daffcdcb11a4a776933bb4cfd94be71778
YeonwooSung <neos960518@gmail.com>
2022-04-05 22:34:46 +0900

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# remove pre-built files
+rm -rf pyretro.egg-info
+rm -rf dist
+rm -rf build
+
+# build by using wheel
 python3 setup.py bdist_wheel
 
+# deploy by using twine
 python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose
